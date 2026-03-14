8c80aa91b0cf66b064b335ceed87bfdb5b98d908
Maximilian Teegen <max@arch-distrobox.haruka>
2026-03-14 22:12:22 +0100

    this crate doesn't need asynchronous locks

--- src/exporter.rs
+++ src/exporter.rs
@@ -1,11 +1,10 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex, RwLock};
 use std::time::Duration;
 
 use opentelemetry_sdk::error::{OTelSdkError, OTelSdkResult};
 use opentelemetry_sdk::metrics::Temporality;
 use opentelemetry_sdk::metrics::data::ResourceMetrics;
 use opentelemetry_sdk::metrics::exporter::PushMetricExporter;
-use tokio::sync::{Mutex, RwLock};
 
 use crate::convert::WriteOpenMetrics;
 
@@ -33,7 +32,11 @@ impl OpenMetricsExporter {
 
     /// Get a clone of the last-exported OpenMetrics text.
     pub async fn text(&self) -> String {
-        self.buffer.read().await.as_str().to_owned()
+        match self.buffer.read().map(|t| t.as_str().to_owned()) {
+            Ok(text) => text,
+            Err(_) => std::future::pending().await,
+            // TODO: turn this into a non-async function in 0.32
+        }
     }
 }
 
@@ -41,7 +44,9 @@ impl PushMetricExporter for OpenMetricsExporter {
     async fn export(&self, metrics: &ResourceMetrics) -> OTelSdkResult {
         #[cfg(feature = "tracing")]
         tracing::debug!("Exporting metrics");
-        let mut backbuffer = self.backbuffer.lock().await;
+        let mut backbuffer = self.backbuffer.lock().map_err(|err| {
+            OTelSdkError::InternalFailure(format!("Failed to acquire backbuffer: {err}"))
+        })?;
         backbuffer.clear();
         metrics
             .write_as_openmetrics(&mut *backbuffer)
@@ -49,7 +54,11 @@ impl PushMetricExporter for OpenMetricsExporter {
                 OTelSdkError::InternalFailure(format!("Failed to write to buffer: {err}"))
             })?;
 
-        let mut frontbuffer = self.buffer.write().await;
+        let mut frontbuffer = self.buffer.write().map_err(|err| {
+            OTelSdkError::InternalFailure(format!(
+                "Failed to acquire frontbuffer for writer: {err}"
+            ))
+        })?;
         std::mem::swap(&mut *frontbuffer, &mut *backbuffer);
 
         Ok(())
